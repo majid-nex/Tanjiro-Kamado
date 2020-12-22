a8b3d661471ab2f5bd295323c50e9de172164d3f
Tanjiro Kamado <69890535+JayPatel1314@users.noreply.github.com>
2020-12-22 22:08:09 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -275,7 +275,7 @@ def info(update: Update, context: CallbackContext):
         text += "\n\nThe Disaster level of this person is 'God'."
         disaster_level_present = True
     elif user.id in DEV_USERS:
-        text += "\n\nThis user is member of 'Bot Lab'."
+        text += "\n\nThis user is member of 'Harshiras'."
         disaster_level_present = True
     elif user.id in DRAGONS:
         text += "\n\nThe Disaster level of this person is 'Dragon'."
@@ -291,7 +291,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/BotLabUpdates/33">?</a>]'.format(
+        text += ' [<a href="https://t.me/TanjiroKamadoSupport/3">?</a>]'.format(
             bot.username)
 
     try:
